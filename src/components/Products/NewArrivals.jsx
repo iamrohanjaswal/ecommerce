d15b82824d85632eb4ec8245d8f349c2d6ffd842
iamrohanjaswal <rohanjaswal45@gmail.com>
2025-07-03 19:32:22 +0530

    scroll buttons

--- src/components/Products/NewArrivals.jsx
+++ src/components/Products/NewArrivals.jsx
@@ -148,14 +148,15 @@ useEffect(()=>{
     if(container){
         container.addEventListener("scroll", updateScrollButtons);
         updateScrollButtons();
+        return () =>container.removeEventListener("scroll", updateScrollButtons);
     }
-})
+},[]);
 
 
 
 
   return (
-    <section>
+    <section className="py-16 px-4 lg:px-0">
         <div className="container mx-auto text-center mb-10 relative">
             <h2 className='text-3xl font-bold mb-4'>
                 Explore New Arrivals
