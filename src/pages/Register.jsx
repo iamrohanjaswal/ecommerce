14ae23fc612a29e3218798c0058126647a5b1597
iamrohanjaswal <rohanjaswal45@gmail.com>
2025-07-07 22:51:33 +0530

    Pofile page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import login from "../assets/login.webp";
+import register from "../assets/register.webp";
 
-const Login = () => {
+const Register = () => {
     const [email, setEmail]=useState("");
     const [password, setPassword]=useState("");
+    const [name, setName]=useState("");
 
     const handleSubmit =(e)=>{
         e.preventDefault();
-        console.log("User Login: ", {email, password});
+        console.log("User Register: ", {name, email, password});
     };
 
   return (
@@ -22,6 +23,18 @@ const Login = () => {
             </h2>
             <h2 className="text-2xl font-bold text-center mb-6"> Hey there!👋</h2>
             <p className="text-center mb-6"> Enter your username and password to Login</p>
+
+
+            <div className="mb-4">
+                <label className="block text-sm font-semibold mb-2">Name</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
+                    className="w-full p-2 border rounded"
+                    placeholder="Enter your Name"
+                />
+                </div>
 
             <div className="mb-4">
                 <label className="block text-sm font-semibold mb-2">Email</label>
@@ -48,11 +61,11 @@ const Login = () => {
                     <div className="mt-6">
                         <button
                             type='submit'className="w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition"
-                        >Sign In</button>
+                        >Sign Up</button>
                     <p className="mt-6 text-center text-sm">
                         Don't have an account?{" "}
-                        <Link to="/register" className="text-blue-500">
-                        Register</Link>
+                        <Link to="/login" className="text-blue-500">
+                        Login</Link>
                     </p>
                     </div>    
                 </div>
@@ -61,11 +74,11 @@ const Login = () => {
 
         <div className='hidden md:block w-1/2 bg-gray-800'>
         <div className="h-full flex flex-col justify-center items-center">
-            <img src={login} alt="" className="h-[750px] w-full object-cover"/>
+            <img src={register} alt="" className="h-[750px] w-full object-cover"/>
         </div>
         </div>
     </div>
   )
 }
 
-export default Login
+export default Register;
